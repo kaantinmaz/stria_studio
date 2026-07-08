76bbf162a21a490c8777a2a8653c5cea3f53e0b7
kaantinmaz <79404689+kaantinmaz@users.noreply.github.com>
2026-07-08 10:31:03 +0300

    feat(admin): Filament Post/Category/Tag resources

---
+++ backend/app/Filament/Resources/Tags/Schemas/TagForm.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Filament\Resources\Tags\Schemas;
+
+use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Schema;
+
+class TagForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('name_tr')
+                    ->required(),
+                TextInput::make('name_en')
+                    ->required(),
+                TextInput::make('slug')
+                    ->required()
+                    ->unique(ignoreRecord: true),
+            ]);
+    }
+}
